f86ca1b6c5f7bc57fc1c981eecbfcfdd9d6049f8
zkhku <94850618+zkhku@users.noreply.github.com>
2021-11-22 10:48:47 -0500

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,18 +2,22 @@ import torch
 root_path= "change_to_your_current_path/"
 
 seed=2021
-
-num_owners = 3
+no_cuda=False
+cuda = not no_cuda and torch.cuda.is_available()
 
 dataset = "cora"
+num_owners = 3
 delta=20
 
 num_samples = [5,5]
-
 batch_size = 32
 latent_dim=128
 steps=10
 epochs_local=1
+lr=0.001
+weight_decay=1e-4
+hidden=32
+dropout=0.5
 
 gen_epochs=20
 num_pred=5
@@ -22,8 +26,6 @@ hidden_portion=0.5
 epoch_classifier=20
 classifier_layer_sizes=[64,32]
 
-
-
 local_test_acc_dir = root_path+'local_result/test_acc/' + dataset+"_"+str(num_owners)
 global_test_acc_file = root_path+'global_result/test_acc/' + dataset +"_"+str(num_owners)+".txt"
 global_classifer_file=root_path+"global_result/model/"+dataset+"_"+str(num_owners)+"classifier.h5"
@@ -31,15 +33,6 @@ local_gen_dir = root_path+'local_result/model/'+dataset+"_"+str(num_owners)
 global_gen_nc_acc_file = root_path+'global_result/test_acc/' + dataset +"_nc_"+str(num_owners)+".txt"
 local_downstream_task_dir=root_path+'local_result/classifier_info/' + dataset+"_"+str(num_owners)
 
-lr=0.001
-weight_decay=1e-4
-hidden=32
-dropout=0.5
-
-no_cuda=False
-cuda = not no_cuda and torch.cuda.is_available()
-
 a=1
 b=1
 c=1
-
